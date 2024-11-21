bf7f7bcc78030d47bb374fbcc3d49a47ef350c8b
Bunmi Oye <bunmioye09@gmail.com>
2024-11-21 21:22:17 +0100

    modify modal to look like contact page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Contact from "./Contact";
 import "./App.css";
 import destinations from "./data/destinations";
 import services from "./data/services";
+import Footer from "./Footer";
 
 function App() {
   const [modalVisible, setModalVisible] = useState(false);
@@ -23,7 +24,6 @@ function App() {
       <Modal open={modalVisible} close={() => setModalVisible(false)}>
         <Contact />
       </Modal>
-
       <header>
         <nav>
           <div>
@@ -101,7 +101,6 @@ function App() {
           </div>
         </div>
       </section>
-
       <section class="bestT">
         <h2>Our Best Tour</h2>
         <p>
@@ -117,21 +116,12 @@ function App() {
               <p>
                 {destination.country}, {destination.city}
               </p>
-              <p>{destination.price}</p>
+              <p>${destination.price}</p>
             </div>
           ))}
         </div>
       </section>
-
-      <footer>
-        <div>
-          <p>Copyright © {new Date().getFullYear()} BagTrip</p>
-        </div>
-        <div>
-          <h2>Address</h2>
-          <p>Nigeria - 4, Fred Omojole Street, Gbagada, Lagos State, Nigeria</p>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 }
--- src/Modal.js
+++ src/Modal.js
@@ -1,7 +1,9 @@
 import close from "./assets/images/close.png";
+import Footer from "./Footer";
 import "./modal.css";
+import banner from "./assets/images/earthmap.png";
 
-const Modal = (props) => {
+const Modal = props => {
   const attachedClasses = props.open
     ? ["open", props.className]
     : ["close", props.className];
@@ -10,12 +12,22 @@ const Modal = (props) => {
 
   return (
     <div className={`modal_container ${backdropClasses.join(" ")}`}>
+      <nav>
+        <div>
+          <p className="logo">BagTrip</p>
+        </div>
+        <div>
+          <button className="btn_primary" onClick={props.close}>
+            Home
+          </button>
+        </div>
+      </nav>
+
       <div className={`modal ${attachedClasses.join(" ")}`}>
-        <span className="close-modal" onClick={props.close}>
-          <img src={close} alt="close" />
-        </span>
         {props.children}
       </div>
+      <img className="banner" src={banner} alt="" />
+      <Footer />
     </div>
   );
 };
